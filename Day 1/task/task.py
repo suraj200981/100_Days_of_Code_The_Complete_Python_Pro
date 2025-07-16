980509aa6834e1f306d32d0e779036816cb63fe4
suraj <suraj200981@yahoo.co.uk>
2025-07-15 18:53:15 -0600

    tip calculator made

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -2,4 +2,4 @@ print("Welcome to the Band Name Generator.")
 userCity = input("What city did you grow up in?")
 userPet = input("What is the name of your pet?")
 bandName = userCity+" "+userPet
-print("Your band name is "+ bandName)
+print(f"Your band name is {bandName}")
--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -2,5 +2,8 @@ print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
+eachPerson = (bill/people) *((tip/100)+1)
+print(f"Each person should pay: {round(eachPerson,2)}" )
+
 
 
