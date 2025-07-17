8dfe15e45aa5469e3b6b093c744d81cf2c6cd41d
suraj <suraj200981@yahoo.co.uk>
2025-07-16 21:50:22 -0600

    python pizza

--- Day 3/If Else/task.py
+++ Day 3/If Else/task.py
@@ -1,2 +1,6 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+if (height > 10):
+    print("You can ride the coaster")
+else:
+    print("You are not tall enough, sorry")
--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -2,4 +2,31 @@ print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
+total = 0
+
+if size== "S":
+    total+=15
+elif size == "M":
+    total+=20
+elif size == "L":
+    total+=25
+else:
+    print("incorrect size")
+
+if size=="L" or size == "M" and pepperoni == "Y":
+    total+=3
+elif size=="S" and pepperoni == "Y":
+    total+=2
+
+if extra_cheese == "Y":
+    total+=1
+print(f"Your final bill is: {total}")
+
+
+
+
+
+
+
+
 
