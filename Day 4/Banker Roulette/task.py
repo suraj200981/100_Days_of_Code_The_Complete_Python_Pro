05c6e520eecacb267371428d00c962c08b27e501
suraj <suraj200981@yahoo.co.uk>
2025-07-21 13:05:06 -0600

    banker roulette: randomness with lists

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,9 @@
+import random
+
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+chosen = random.randint(0, len(friends)-1)
+
+print("I choose: "+ friends[chosen])
+
+
+# or you could have done: random.choice(friends)
