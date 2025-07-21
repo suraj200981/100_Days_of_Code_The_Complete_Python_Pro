6ee4dce8d1424356a0a573368440699b4007f924
suraj <suraj200981@yahoo.co.uk>
2025-07-21 13:41:18 -0600

    rock, paper, scissors game

--- Day 4/IndexError/task.py
+++ Day 4/IndexError/task.py
@@ -6,4 +6,6 @@ states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Conne
                      "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma",
                      "New Mexico", "Arizona", "Alaska", "Hawaii"]
 
+print(len(states_of_america))
 print(states_of_america)
+
--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,3 +1,11 @@
+#rules:
+# Scissors wins against paper
+# paper wins against rock
+# rock wins against scissors
+
+
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +32,79 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+list = ["Rock", "Scissors", "Paper"]
+userChoice= int (input("What do you choose? rock = 0, paper = 1, scissors = 2"))
+if random.choice(list) == list[0] and userChoice==0:
+    print("Your choice is rock: ")
+    print(rock)
+    print("")
+    print("Computer choice is rock: ")
+    print(rock)
+    print("")
+    print("Its a draw!")
+elif random.choice(list) == list[0] and userChoice == 1:
+    print("Your choice is paper: ")
+    print(paper)
+    print("")
+    print("Computer choice is rock: ")
+    print(rock)
+    print("")
+    print("Paper beats rock, you win!")
+elif random.choice(list) == list[0] and userChoice==2:
+    print("Your choice is scissors: ")
+    print(scissors)
+    print("")
+    print("Computer choice is rock: ")
+    print(rock)
+    print("")
+    print("Rock beats scissors, you lose!")
+elif random.choice(list) == list[1] and userChoice == 0:
+    print("Your choice is rock: ")
+    print(rock)
+    print("")
+    print("Computer choice is paper: ")
+    print(paper)
+    print("")
+    print("Paper beats rock, you lose!")
+elif random.choice(list) == list[1] and userChoice == 1:
+    print("Your choice is paper: ")
+    print(paper)
+    print("")
+    print("Computer choice is paper: ")
+    print(paper)
+    print("")
+    print("Its a draw!")
+elif random.choice(list) == list[1] and userChoice == 2:
+    print("Your choice is scissors: ")
+    print(scissors)
+    print("")
+    print("Computer choice is paper: ")
+    print(paper)
+    print("")
+    print("Scissors beats paper, you win!")
+elif random.choice(list) == list[2] and userChoice == 0:
+    print("Your choice is rock: ")
+    print(rock)
+    print("")
+    print("Computer choice is scissors: ")
+    print(scissors)
+    print("")
+    print("Rock beats scissors, you win!")
+elif random.choice(list) == list[2] and userChoice == 1:
+    print("Your choice is paper: ")
+    print(paper)
+    print("")
+    print("Computer choice is scissors: ")
+    print(scissors)
+    print("")
+    print("Scissors beats paper, you lose!")
+elif random.choice(list) == list[2] and userChoice == 2:
+    print("Your choice is paper: ")
+    print(scissors)
+    print("")
+    print("Computer choice is scissors: ")
+    print(scissors)
+    print("")
+    print("Its a draw!")
+
+
