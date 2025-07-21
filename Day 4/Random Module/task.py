479f3fd92228785f3dcb6d89309c7e07902231f3
suraj <suraj200981@yahoo.co.uk>
2025-07-21 12:54:54 -0600

    heads or tails app

--- Day 4/Random Module/task.py
+++ Day 4/Random Module/task.py
@@ -1,7 +1,17 @@
 import random
 import mymodule
 
-num = random.randint(1,10)
+#  num = random.randint(1,10)
+# num = random.random()
+#
+# print(f"Random number: {num}")
+# print(f"Favourite number: {mymodule.myFavouriteNumber}")
 
-print(f"Random number: {num}")
-print(f"Favourite number: {mymodule.myFavouriteNumber}")
+# heads or tails
+
+randomVal = random.randint(0, 1)
+
+if randomVal%2==0:
+    print("Heads")
+else:
+    print("Tails")
