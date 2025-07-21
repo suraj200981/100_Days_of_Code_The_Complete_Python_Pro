6c55598d9fa262c11ca538d14d17669af4d906cc
suraj <suraj200981@yahoo.co.uk>
2025-07-21 14:28:49 -0600

    highest score without using prebuilt function

--- Day 5/Highest Score/task.py
+++ Day 5/Highest Score/task.py
@@ -1,2 +1,11 @@
-student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
-print(range(1, 10))
+new_score= [8,65,89,86,55,91,64,89]
+max = 0
+
+for i in new_score:
+    if max > i:
+        continue
+    else:
+        max=i
+
+
+print(max)
